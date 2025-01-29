76d4aa361a5415876b0252395928a3caa7822609
TRUPTI1203 <truptimayeesuar@gmail.com>
2025-01-30 01:54:39 +0530

    comparison added

---
+++ comparison.js
@@ -0,0 +1,21 @@
+/* console.log(3 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1); */
+
+/* console.log("2" > 1);
+console.log("02" > 1); */
+
+/* console.log(null > 0); //false
+console.log(null == 0);  //false
+console.log(null >= 0); */ //true
+// the reason == and ><>=<= work differently is that the comparison operators convert null to 0
+
+/* console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0); */
+
+// === strictly compares
+
+console.log("2" === 2); //false as datatypes are different

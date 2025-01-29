49742a5c320d0081046b6c589d008052bc75dc55
TRUPTI1203 <truptimayeesuar@gmail.com>
2025-01-30 00:27:51 +0530

    conversion

---
+++ basics.js
@@ -0,0 +1,31 @@
+//let score = "trupti";
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//for "33abc" it gave NaN
+//for null it gave 0
+//for undefined it gave NaN
+//for true it gave 1
+//for false it gave 0
+//for a string it gave NaN(not a number)
+
+//let isLoggedIn = "";
+
+/*let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+1 => true 0 => false
+"" => false
+"trupti" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
